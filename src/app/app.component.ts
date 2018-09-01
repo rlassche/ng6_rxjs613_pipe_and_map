bbe6d1531a7898b7df9f03f6df9880a8b80306ca
Your Name <you@example.com>
2018-09-01 12:46:37 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpResponse } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stack';
+
+  constructor(private http: HttpClient) {
+    this.simple1().subscribe(
+      (response:HttpResponse<any>) => { console.log(response)},
+      (error:HttpErrorResponse) => { console.log("error", error)},
+      () => { console.log( 'finish')}
+    )
+  }
+  simple1(): Observable<any> {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos').pipe(
+      map((res:[any]) => {
+        let newArray:[any] = <any>[] ;
+        res.forEach( (e) => {
+          if( e['title'][0] == 'a') {
+              newArray.push( res[0])
+          }
+        })
+        return newArray
+      })
+    )
+  }
 }
